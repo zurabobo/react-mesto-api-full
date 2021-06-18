ae187764f0a73e8dfb31ae3c70d73be56ecb0441
Zura <zuramariwedding@gmail.com>
2021-06-18 19:54:11 +0300

    sprint 15

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -109,7 +109,7 @@ class Api {
 const api = new Api({
     baseUrl: 'https://api.zb.students.nomoredomains.club',
     headers: {
-      authorization: '9da2e5f7-acdb-422d-9138-e514aeb3e40e',
+      'Accept': 'application/json',
       'Content-Type': 'application/json'
     }
   });
