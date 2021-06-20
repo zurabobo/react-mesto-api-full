15700b8b8a3e630cb3130d768857cc7bbcafe1b1
Zura <zuramariwedding@gmail.com>
2021-06-20 11:06:24 +0300

    sprint 15

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -7,23 +7,23 @@ function Card({ card, onCardClick, onCardDeleteConfirm, onCardLike }) {
 
   const currentUser = useContext(CurrentUserContext);
 
-  const isOwn = card.owner._id === currentUser._id;
-  // const cardDeleteButtonClassName = isOwn ? 
-  //   'card__delete-button button-opacity card__delete-button_visible' : 'card__delete-button button-opacity card__delete-button_hidden'
-  // ;
+  const isOwn = card.owner === currentUser._id;
+  const cardDeleteButtonClassName = isOwn ? 
+    'card__delete-button button-opacity card__delete-button_visible' : 'card__delete-button button-opacity card__delete-button_hidden'
+  ;
 
-  // const isLiked = card.likes.find(i => i._id === currentUser._id);
-  // const cardLikeButtonClassName = isLiked ? 
-  //   'card__like-button button-opacity card__like-button_active' : 'card__like-button button-opacity'
-  // ;
-  const cardDeleteButtonClassName = (
-    `card__delete-button button-opacity ${isOwn ? 'card__delete-button_visible' : 'card__delete-button_hidden'}`
-  );
+  const isLiked = card.likes.find(i => i === currentUser._id);
+  const cardLikeButtonClassName = isLiked ? 
+    'card__like-button button-opacity card__like-button_active' : 'card__like-button button-opacity'
+  ;
+  // const cardDeleteButtonClassName = (
+  //   `card__delete-button button-opacity ${isOwn ? 'card__delete-button_visible' : 'card__delete-button_hidden'}`
+  // );
 
-  const isLiked = card.likes.some(i => i._id === currentUser._id);
-  const cardLikeButtonClassName = (
-    `card__like-button button-opacity ${isLiked ? 'card__like-button_active' : 'card__like-button button-opacity'}`
-  );
+  // const isLiked = card.likes.some(i => i._id === currentUser._id);
+  // const cardLikeButtonClassName = (
+  //   `card__like-button button-opacity ${isLiked ? 'card__like-button_active' : 'card__like-button button-opacity'}`
+  // );
 
   function handleClick() {
     onCardClick(card);
