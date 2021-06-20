1e65d1a27561054861d8153ec45f5c3cdbfcede6
Zura <zuramariwedding@gmail.com>
2021-06-20 11:20:01 +0300

    sprint 15

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -12,7 +12,7 @@ function Card({ card, onCardClick, onCardDeleteConfirm, onCardLike }) {
     'card__delete-button button-opacity card__delete-button_visible' : 'card__delete-button button-opacity card__delete-button_hidden'
   ;
 
-  const isLiked = card.likes.some(i => i === currentUser._id);
+  const isLiked = card.likes.find(i => i === currentUser._id);
   const cardLikeButtonClassName = isLiked ? 
     'card__like-button button-opacity card__like-button_active' : 'card__like-button button-opacity'
   ;
