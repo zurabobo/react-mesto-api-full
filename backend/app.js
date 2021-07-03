100a46e5c9809b3098a2894a680f6351826e4466
Zura <zuramariwedding@gmail.com>
2021-07-03 17:00:30 +0300

    remove crash-test

--- backend/app.js
+++ backend/app.js
@@ -51,12 +51,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
