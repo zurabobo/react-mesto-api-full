398565e9fbb6c9cec7408886a7abb899da9d3b67
Zura <zuramariwedding@gmail.com>
2022-07-08 01:10:32 +0400

    deploy on heroku

--- app.js
+++ app.js
@@ -72,15 +72,15 @@ app.use(requestLogger);
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static(path.resolve(__dirname, 'frontend/build')));
   app.get('*', (req, res) => {
     // eslint-disable-next-line no-global-assign
-    res.sendFile(path.join(__dirname = 'frontend/build/index.html'));
+    res.sendFile(path.resolve(__dirname = 'frontend/build/index.html'));
   });
 }
 app.get('*', (req, res) => {
   // eslint-disable-next-line
-  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+  res.sendFile(path.resolve(__dirname+'/frontend/build/index.html'));
 });
 
 // app.use(express.static(path.join(__dirname, './frontend/build')));
