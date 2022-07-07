9d991b0c7ff7dfb6d2802a8007eeb19b392a5228
Zura <zuramariwedding@gmail.com>
2022-07-07 23:36:05 +0400

    deploy on heroku

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(requestLogger);
 // app.use(express.static(path.join(__dirname, './frontend/build')));
 
 if (process.env.NODE_ENV) {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('./frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
