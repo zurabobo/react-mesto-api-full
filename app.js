189f5fb4792b1224dff77423c667673617e8fc56
Zura <zuramariwedding@gmail.com>
2022-07-12 00:45:34 +0400

    heroku deploy

--- app.js
+++ app.js
@@ -69,22 +69,19 @@ app.use(requestLogger);
 // const buildPath = path.join(__dirname, 'build');
 // app.use(express.static(buildPath));
 
-// eslint-disable-next-line
-app.use(express.static(path.join(__dirname + "public")));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-// app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.resolve(__dirname, 'frontend/build')));
-//   app.get('*', (req, res) => {
-//     // eslint-disable-next-line no-global-assign
-//     res.sendFile(path.resolve(__dirname = 'frontend/build/index.html'));
-//   });
-// }
-// app.get('*', (req, res) => {
-//   // eslint-disable-next-line
-//   res.sendFile(path.resolve(__dirname+'/frontend/build/index.html'));
-// });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'frontend/build')));
+  app.get('*', (req, res) => {
+    // eslint-disable-next-line no-global-assign
+    res.sendFile(path.resolve(__dirname = 'frontend/build/index.html'));
+  });
+}
+app.get('*', (req, res) => {
+  // eslint-disable-next-line
+  res.sendFile(path.resolve(__dirname+'/frontend/build/index.html'));
+});
 
 // app.use(express.static(path.join(__dirname, './frontend/build')));
 // app.get('*', (req, res) => {
