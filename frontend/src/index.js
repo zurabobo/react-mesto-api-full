f001e7c930b202dde94d16cf99dd736a8f15fbf8
Zura <zuramariwedding@gmail.com>
2022-09-20 12:48:13 +0400

    serviceWorker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,10 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-registerServiceWorker()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
